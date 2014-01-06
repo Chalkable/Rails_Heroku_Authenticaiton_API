d872c02f7acf10f3fc5da6fa93715cff5bb67936
jforaker <jake@chalkable.com>
2014-01-06 17:41:48 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChlkLogin::Application.config.secret_key_base = 'b1afeb2725c52f01a1b47934732b7d188cd04a500352a4246633e574cb84bd747b9c44772cb53af1b739459046e08501c8c94112cadf3517b656dc7d3cf72502'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChlkLogin::Application.config.session_store :cookie_store, key: '_chlk_login_session'
